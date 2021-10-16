1f43bd086d64ec8278c7ead4a439562873827eee
Devon Artis <562122-devonartis@users.noreply.gitlab.com>
2021-10-16 13:42:45 -0400

    Updated contract and run function

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,20 @@ pragma solidity ^0.8.4;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
+
     constructor() {
         console.log("Yo yo, I am a contract and I am very smart");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved %d times", msg.sender, totalWaves);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        //console.log("%s has waved %d times", msg.sender, totalWaves);
+        console.log("we have waved %d times", totalWaves);
+        return totalWaves;
     }
 }
