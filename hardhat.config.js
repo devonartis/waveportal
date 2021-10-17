ad72e8f6ddf6f4c707d07a4935fa8fe348aadd66
Devon Artis <562122-devonartis@users.noreply.gitlab.com>
2021-10-17 13:13:05 -0400

    Updated hardhat to support rinkeby network

--- hardhat.config.js
+++ hardhat.config.js
@@ -14,9 +14,22 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+
+const { API_URL, PRIVATE_KEY } = process.env;
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.8.4",
+  defaultNetwork: "rinkeby",
+   networks: {
+      hardhat: {},
+      rinkeby: {
+         url: API_URL,
+         accounts: [`0x${PRIVATE_KEY}`]
+      }
+   },
 };
